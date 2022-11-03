34ff627ab25383cde97410dabd46b003919f29ba
Imdad Miran <abdullahbinrashed1225@gmail.com>
2022-11-03 14:06:14 +0600

    Update You_either_know_XOR_you_dont.py

--- Introduction-To-Cryptohack/You_either_know_XOR_you_dont.py
+++ Introduction-To-Cryptohack/You_either_know_XOR_you_dont.py
@@ -2,6 +2,11 @@ import binascii
 from pwn import *
 
 encoded = binascii.unhexlify("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
+hint = "crypto{}"
+hint_byte = hint.encode()
+hintt = xor(encoded, hint_byte)
+print(hintt)
+
 key = "myXORkey"
 key_byte = key.encode()
 
